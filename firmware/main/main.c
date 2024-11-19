f82edcf3fbc5c3e657f72ca2884d36a17b12636d
deimos <deimoshall@gmail.com>
2024-11-19 09:13:20 -0600

    feat: add leds animation at startup

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -21,9 +21,12 @@
 #include "wifi_app.h"
 
 // Define the GPIO pins for the Red, Green, and Blue LEDs
-#define GPIO_LED_RED   2
-#define GPIO_LED_GREEN 15
-#define GPIO_LED_BLUE  3
+#define GPIO_LED_RED_1   2
+#define GPIO_LED_GREEN_1 15
+#define GPIO_LED_BLUE_1  3
+#define GPIO_LED_RED_2   11
+#define GPIO_LED_GREEN_2 10
+#define GPIO_LED_BLUE_2  1
 
 // Define colors as RGB values in hexadecimal
 #define RED      0x00FFFF  // Red color for common anode (invert the bits)
@@ -46,54 +49,28 @@ void app_main() {
 
   // Create an instance of the RGB LED structure and initialize it with the
   // defined GPIO pins and LEDC channels
-  rgb_led_t led1 = rgb_led_new(GPIO_LED_RED, GPIO_LED_GREEN, GPIO_LED_BLUE,
-                               LEDC_CHANNEL_0, LEDC_CHANNEL_1, LEDC_CHANNEL_2);
+  rgb_led_t led1 =
+      rgb_led_new(GPIO_LED_RED_1, GPIO_LED_GREEN_1, GPIO_LED_BLUE_1,
+                  LEDC_CHANNEL_0, LEDC_CHANNEL_1, LEDC_CHANNEL_2);
+  rgb_led_t led2 =
+      rgb_led_new(GPIO_LED_RED_2, GPIO_LED_GREEN_2, GPIO_LED_BLUE_2,
+                  LEDC_CHANNEL_3, LEDC_CHANNEL_4, LEDC_CHANNEL_5);
   //...you can add one more eg: led2.It is limited to 2 leds due to the LEDC
   // module having only 8 channels available.
 
   // Initialize the RGB LED, check for any errors during the process
   ESP_ERROR_CHECK(rgb_led_init(&led1));
+  ESP_ERROR_CHECK(rgb_led_init(&led2));
 
-  // // Start a "breathing" effect with the Red color and a period of 100ms
-  // ESP_LOGI(TAG, "Starting breathing effect with Red color");
-  // rgb_led_start_breath_effect(&led1, RED, 100);
-  // vTaskDelay(pdMS_TO_TICKS(5000));  // Delay for 5 seconds
-
-  // // Start a blinking effect with the Green color. Blink 3 times, each blink
-  // // lasts 500ms, and the time between blinks is 500ms. The total effect
-  // // duration is 3000ms.
-  // ESP_LOGI(TAG, "Starting blinking effect with Green color");
-  // rgb_led_start_blink_effect(&led1, GREEN, 3, 500, 500, 3000);
-
-  // // Start a "breathing" effect with the Blue color and a period of 100ms
-  // ESP_LOGI(TAG, "Starting breathing effect with Blue color");
-  // rgb_led_start_breath_effect(&led1, BLUE, 100);
-  // vTaskDelay(pdMS_TO_TICKS(5000));  // Delay for 5 seconds
-
-  // Start a blinking effect with the Yellow color. Blink 3 times, each blink
-  // lasts 500ms, and the time between blinks is 500ms. The total effect
-  // duration is 3000ms.
-  ESP_LOGI(TAG, "Starting blinking effect with Yellow color");
-  rgb_led_start_blink_effect(&led1, BLUE, 3, 500, 500, 3000);
-  vTaskDelay(pdMS_TO_TICKS(5000));  // Delay for 5 seconds
-
-  // Start a color transition effect from Yellow to Red, the total effect
-  // duration is 5000ms.
-  // ESP_LOGI(TAG, "Starting color transition effect from Yellow to Red");
-  // rgb_led_start_transition_effect(&led1, YELLOW, RED, 5000);
-  // vTaskDelay(pdMS_TO_TICKS(5000));  // Delay for 5 seconds
-
-  // Set the LED color Fixed to Violet
-  ESP_LOGI(TAG, "Setting LED color to Violet");
-  rgb_led_set_color(&led1, VIOLET);
-  vTaskDelay(pdMS_TO_TICKS(5000));  // Delay for 5 seconds
-  ESP_LOGI(TAG, "Setting LED color to RED");
-  rgb_led_set_color(&led1, RED);
-  vTaskDelay(pdMS_TO_TICKS(5000));  // Delay for 5 seconds
-  ESP_LOGI(TAG, "Turning off the LED");
-  rgb_led_set_color(&led1, NO_COLOR);
-  vTaskDelay(pdMS_TO_TICKS(5000));  // Delay for 5 seconds
-  rgb_led_set_color(&led1, YELLOW);
+  for (uint8_t i = 0; i < 3; i++) {
+    rgb_led_set_color(&led1, RED);
+    rgb_led_set_color(&led2, NO_COLOR);
+    vTaskDelay(pdMS_TO_TICKS(200));
+    rgb_led_set_color(&led1, NO_COLOR);
+    rgb_led_set_color(&led2, BLUE);
+    vTaskDelay(pdMS_TO_TICKS(200));
+  }
+  rgb_led_set_color(&led2, NO_COLOR);
 
   // flash_fs_begin(flash_fs_screens_handler);
   // sd_card_begin();
